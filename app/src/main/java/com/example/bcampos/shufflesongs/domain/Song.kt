678163135f95ebd0fe95fdd287c1d8678932781e
bruno campos <camposbrunocampos@gmail.com>
2019-02-24 23:36:04 -0300

    layout adjusts ;

--- app/src/main/java/com/example/bcampos/shufflesongs/domain/Song.kt
+++ app/src/main/java/com/example/bcampos/shufflesongs/domain/Song.kt
@@ -1,3 +1,9 @@
 package com.example.bcampos.shufflesongs.domain
 
-data class Song(val artistName: String = "", val trackName: String = "", val wrapperType: String = "", val artworkUrl: String = "")
+data class Song(
+    val artistName: String = "",
+    val trackName: String = "",
+    val wrapperType: String = "",
+    val artworkUrl: String = "",
+    val primaryGenreName: String = ""
+)
--- app/src/main/java/com/example/bcampos/shufflesongs/ui/songslist/SongViewHolder.kt
+++ app/src/main/java/com/example/bcampos/shufflesongs/ui/songslist/SongViewHolder.kt
@@ -1,15 +1,23 @@
 package com.example.bcampos.shufflesongs.ui.songslist
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bcampos.shufflesongs.R
 import com.example.bcampos.shufflesongs.domain.Song
+import com.squareup.picasso.Picasso
 
 class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val trackNameText = itemView.findViewById<TextView>(R.id.track_name)
+    private val trackImage = itemView.findViewById<ImageView>(R.id.track_image)
+    private val trackArtist = itemView.findViewById<TextView>(R.id.track_artist)
+    private val trackGender = itemView.findViewById<TextView>(R.id.track_gender)
 
     fun updateSong(song: Song) {
         trackNameText.text = song.trackName
+        Picasso.get().load(song.artworkUrl).into(trackImage)
+        trackArtist.text = song.artistName
+        trackGender.text = String.format("(%s)", song.primaryGenreName)
     }
 }
