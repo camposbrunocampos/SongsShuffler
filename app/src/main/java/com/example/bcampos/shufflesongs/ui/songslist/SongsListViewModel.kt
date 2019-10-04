f99e3f20aaf20b2048f26b0c2fd43b956d7ddfa9
Bruno Campos <bruno.campos@ehi.com>
2019-10-04 12:57:23 -0300

    refactoring SongsListViewModel to cancel coroutines properly

--- app/src/main/java/com/example/bcampos/shufflesongs/ui/songslist/SongsListViewModel.kt
+++ app/src/main/java/com/example/bcampos/shufflesongs/ui/songslist/SongsListViewModel.kt
@@ -2,11 +2,14 @@ package com.example.bcampos.shufflesongs.ui.songslist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bcampos.shufflesongs.data.State
 import com.example.bcampos.shufflesongs.domain.Shuffler
 import com.example.bcampos.shufflesongs.domain.Song
 import com.example.bcampos.shufflesongs.domain.SongsUseCase
-import com.example.bcampos.shufflesongs.data.State
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 
@@ -17,10 +20,10 @@ class SongsListViewModel(private val songsUseCase: SongsUseCase, override val co
         MutableLiveData<State<List<Song>>>()
     }
 
-    private lateinit var job: Job
+    private val job = Job()
 
     fun loadSongs() {
-        job = launch {
+        launch {
             songsState.postValue(
                 State(
                     State.Name.LOADING,
@@ -42,7 +45,7 @@ class SongsListViewModel(private val songsUseCase: SongsUseCase, override val co
             State.Name.LOADING,
             songsState.value?.value
         )
-        GlobalScope.launch {
+        launch {
             delay(1000)
             val shuffledSongs = Shuffler.shuffle(songsState.value?.value!!, false)
             songsState.postValue(
